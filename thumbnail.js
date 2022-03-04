0d693039d0350c1aa414f594693f9f8ef9ffadbf
JohnZhang1998 <89688799+JohnZhang1998@users.noreply.github.com>
2022-03-04 10:25:42 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
  
-   targetImage.classList.remove("small");
+   targetImage.classList.remove("big");
 });
     
 });
